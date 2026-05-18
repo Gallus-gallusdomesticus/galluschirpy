4f5bd535dca83ab5d632b95e346d0a48566629ec
Gallus-gallusdomesticus <stephenefelson1999@gmail.com>
2026-05-18 15:36:46 +1000

    Add handlercounter and handlerreset

--- handler.go
+++ handler.go
@@ -1,10 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 
+}
+
+func (cfg *apiConfig) handlerCounter(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	result := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	w.Write([]byte(result))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	cfg.fileserverHits.Swap(0)
+	result := fmt.Sprintf("Hit counter reset: %d", cfg.fileserverHits.Load())
+	w.Write([]byte(result))
 }
--- main.go
+++ main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	const filePathRoot = "."
+	const port = "8080"
+
+	handle := http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))
+	apiCfg := apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handle))
 	server := &http.Server{}
-	server.Addr = ":8080"
+	server.Addr = ":" + port
 	server.Handler = mux
 
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerCounter)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
 }
